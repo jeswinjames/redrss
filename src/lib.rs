26b847fa61396b991a3cca440cec0cd87c5fe085
arv1ndh <arvindhium@gmail.com>
2020-07-09 09:10:26 -0700

    created function that crafts the url based on the provided config

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,36 @@
 use serde::Deserialize;
 use serde_json;
+//use reqwest::blocking::Client;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 enum Post {
     New,
     Hot,
     Top
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Rss {
-    reddit_secret: String,
+    subreddit: String,
     post_type: Post,
     no_of_post: u8,
     webhook: String
 }
 
 impl Rss {
-    fn new(json_object: &str) -> Rss {
+    pub fn new(json_object: &str) -> Rss {
         let r: Rss = serde_json::from_str(json_object).unwrap();
         r
     }
 }
+
+pub fn url_crafter(subreddit: &str, post_type: &str, num: &str) -> String {
+    let url = format!("https://reddit.com/r/{}/{}/.json?count={}", subreddit, post_type, num);
+    url.to_owned()
+}
+
+//pub fn red_requester(subreddit: &str, post_type: &str, num: &str) -> String {
+//}
 
 #[cfg(test)]
 mod tests {
@@ -30,13 +39,20 @@ mod tests {
     #[test]
     fn test_new() {
         let sample_json = r#"{
-                            "reddit_secret": "Test",
+                            "subreddit": "Test",
                             "post_type": "Hot",
                             "no_of_post": 10,
                             "webhook": "samp"
                             }"#;
 
         let test_r = Rss::new(&sample_json);
-        assert_eq!(test_r.reddit_secret, "Test");
+        assert_eq!(test_r.subreddit, "Test");
+    }
+
+    #[test]
+    fn test_url_crafter() {
+        let hardcoded_url = "https://reddit.com/r/rust/top/.json?count=1";
+        let obtained_url = url_crafter("rust", "top", "1");
+        assert_eq!(hardcoded_url, obtained_url);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use redrss::Rss;
+use std::fs;
+
+pub const CONFIG_FILE: &str = "config";
+
 fn main() {
-    println!("Hello, world!");
+    let json_object = fs::read_to_string(CONFIG_FILE).expect("File Read failed");
+    let rss_object = Rss::new(&json_object);
+    println!("----------------> {:?}", rss_object);
 }
